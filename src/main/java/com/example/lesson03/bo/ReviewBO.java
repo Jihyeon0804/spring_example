e871200ba59cf83183785286efe16679a494a8a2
학원 <test@test.com>
2023-09-25 21:46:50 +0900

    Spring DB 연동 4

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -11,9 +11,24 @@ public class ReviewBO {
 	
 	@Autowired
 	private ReviewMapper reviewMapper;
+	
+	// 조회하기(select)
 	// input : id
 	// output : Review(단건)
 	public Review getReview(int id) {
 		return reviewMapper.selectReview(id);
+	}
+	
+	// insert
+	// input : Review(단건)
+	// output : int(insert가 성공된 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "김비다", 5.0, "역시 맛있다!!!")
+	public int addReviewAsField(int storeId, String menu, String userName,
+							Double point, String review) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
 	}
 }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -7,7 +8,21 @@ import com.example.lesson03.domain.Review;
 @Repository
 public interface ReviewMapper {
 	
-	//input : id
+	// select
+	// input : id
 	// output : Review(단건)
 	public Review selectReview(int id);
+	
+	
+	// insert
+	// return int는 MyBatis가 성공된 행의 개수를 리턴해준다. (MyBatis의 기능임)
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			// @Param => 파라미터 2개 이상이면 map으로 만든다
+			@Param("storeId") int storeId,
+			@Param("menu") String menu,
+			@Param("userName") String userName,
+			@Param("point") Double point,
+			@Param("review") String review);
 }
